2b3457100bcea1311c776740a9194d52cc316dea
umeda-skyley <hidekazu.umeda@skyley.com>
2017-09-21 10:11:25 +0900

    change copyright

--- skwan/Project/source/main.c
+++ skwan/Project/source/main.c
@@ -1,7 +1,17 @@
-/*+ 
-*   COPYRIGHT(C) 2014 LAPIS SEMICONDUCTOR CO., LTD.
-*   ALL RIGHT RESERVED
-*   Author : (LAPIS Semiconductor Inc.)
+/**
+    Author:       Skyley Networks, Inc.
+    Version:
+    Description:  Scalable-Slotted MAC (aka SSMAC) for SkWAN
+    
+    Copyrights(C) 2016-2017 Skyley Networks, Inc. All Rights Reserved.
+
+    This program and the accompanying materials are made available under 
+    the terms of the Eclipse Public License v1.0 which accompanies this 
+    distribution, and is available at
+    http://www.eclipse.org/legal/epl-v10.html
+
+    Contributors:
+    Skyley Networks, Inc. - initial API, implementation and documentation
 */
 #include "compiler_options.h"
 
